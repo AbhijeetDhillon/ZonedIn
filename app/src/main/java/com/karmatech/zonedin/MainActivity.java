453e4d5e4dbf43c4a1f428d8b29fbd365faf2032
AbhijeetDhillon <asdhillon786@gmail.com>
2020-06-26 17:18:31 -0400

    Sleep mode done

--- app/src/main/java/com/karmatech/zonedin/MainActivity.java
+++ app/src/main/java/com/karmatech/zonedin/MainActivity.java
@@ -1,14 +1,94 @@
 package com.karmatech.zonedin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences sharedpreferences;
+    static String MyPREFERENCES = "Modes";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        if (Settings.Secure.getString(this.getContentResolver(),"enabled_notification_listeners").contains(getApplicationContext().getPackageName()))
+        {
+
+        } else {
+            //service is not enabled try to enabled by calling...
+            startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+        }
+
+        Switch sleepMode = findViewById(R.id.sleepModeSwitch);
+        if(sharedpreferences!=null && sharedpreferences.getBoolean("sleepModeOn",false))
+        {
+            sleepMode.setChecked(true);
+        }
+        final Intent notificationIntent = new Intent(getApplicationContext(),MainActivity.class);
+        sleepMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Switch On
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "YOUR_CHANNEL_ID")
+                        .setSmallIcon(R.drawable.sleep) // notification icon
+                        .setContentTitle("ZonedIn") // title for notification
+                        .setContentText("Sleep Mode On")// message for notification
+                        .setAutoCancel(false) // clear notification after click
+                        .setOngoing(true);
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("YOUR_CHANNEL_ID",
+                            "YOUR_CHANNEL_NAME",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+                    channel.setDescription("YOUR_NOTIFICATION_CHANNEL_DISCRIPTION");
+                    mNotificationManager.createNotificationChannel(channel);
+                }
+
+                if(isChecked){
+
+                    PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0,notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    mBuilder.setContentIntent(pi);
+                    mNotificationManager.notify(0, mBuilder.build());
+
+
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putBoolean("sleepModeOn", true);
+                    editor.apply();
+                    Toast.makeText(getApplicationContext(),"Sleep Mode On : Notifications Paused",Toast.LENGTH_LONG).show();
+                }
+                //Switch Off
+                else{
+                    //alert dialog of missed notifications if HashSet is not empty;
+                    mBuilder.setAutoCancel(true);
+                    mNotificationManager.cancelAll();
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putBoolean("sleepModeOn", false);
+                    editor.apply();
+                    Toast.makeText(getApplicationContext(),"Sleep Mode Off : Notifications Resumed",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    public void Custom(View view) {
+        Intent intent = new Intent(this,Customize.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/karmatech/zonedin/Notifications.java
@@ -0,0 +1,51 @@
+package com.karmatech.zonedin;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import java.util.HashSet;
+
+import static android.content.ContentValues.TAG;
+
+@SuppressLint("OverrideAbstract")
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class Notifications extends NotificationListenerService {
+    HashSet<String> set;
+    SharedPreferences sharedPreferences;
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        final String packageName = sbn.getPackageName();
+        sharedPreferences = getSharedPreferences(MainActivity.MyPREFERENCES,Context.MODE_PRIVATE);
+        if(sharedPreferences!=null && sharedPreferences.getBoolean("sleepModeOn",false) && !packageName.contains("com.karmatech")){
+
+            set = new HashSet<>();
+            set.add(packageName);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                cancelNotification(sbn.getPackageName(), sbn.getTag(), sbn.getId());
+            }
+            else {
+                cancelNotification(sbn.getKey());
+            }
+        }
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        // Nothing to do
+        Log.d(TAG, "onNotificationRemoved: Notification removed for" + sbn.getPackageName());
+    }
+}
